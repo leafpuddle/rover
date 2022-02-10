8b0ce0a9df6b1112be9a5be89234bec9fab0062f
Vi Roesler <vroesler@leafpuddle.com>
2022-02-10 14:24:25 -0600

    Altered verbiage of intro command and list command to show option to request features

--- Modules/Information/ModuleIntro.cs
+++ Modules/Information/ModuleIntro.cs
@@ -15,10 +15,15 @@ namespace Rover.Modules
                 Description = 
                     "Hi! I'm Rover.\n" +
                     "I'm a bot built specifically for this Discord server!\n" +
-                    "I have quite a few different tricks I can do.\n\n" +
+                    "I have a few different tricks I can do.\n\n" +
                     
                     "To get started, type !list to list out the different commands I respond to.\n" +
-                    "If you ever have questions about how a command works, you can type !help [command] to see the manual.\n" +
+                    "If you ever have questions about how a command works, you can type !help [command] to see the manual.\n\n" +
+
+                    "I'm very simple right now, but I encourage you to request new features.\n" +
+                    "To make a request, simply let Finch or Vi know.\n" +
+                    "If you want to get involved with my development, let us know!\n\n" +
+
                     "It's nice to meet you! Happy chatting!",
                 Color = 0x419BC4,
                 Footer = new EmbedFooterBuilder { Text = $"Response to {((IGuildUser)Context.User).Nickname ?? Context.User.Username}" }
