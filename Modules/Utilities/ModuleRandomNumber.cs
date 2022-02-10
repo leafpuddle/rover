24413b4b100da8329ec772e611a460c82d4e1fc3
Vi Roesler <vroesler@leafpuddle.com>
2022-02-10 17:53:03 -0600

    Cleaned up FlipCoin and RandomNumber classes, adjusted look of manuals.

--- Modules/Utilities/ModuleRandomNumber.cs
+++ Modules/Utilities/ModuleRandomNumber.cs
@@ -9,80 +9,91 @@ namespace Rover.Modules
         [Summary("Returns a random number between 1 and 100.")]
         public async Task RandomNumber()
         {
-            Random rand = new Random(int.Parse(DateTime.Now.ToString("MMddHHmmss")));
-            var msgembed = new EmbedBuilder
-            {
-                Title = ":grey_question: Random Number - 1 to 100",
-                Description = ((rand.Next() % 100) + 1).ToString(),
-                Color = 0x9E845d,
-                Footer = new EmbedFooterBuilder { Text = $"Response to {((IGuildUser)Context.User).Nickname ?? Context.User.Username}" }
-            };
-            await ReplyAsync(embed: msgembed.Build());
+            int result = GenerateResult(1, 100);
+
+            await ReplyAsync(embed: GenerateEmbed(1, 100, result));
         }
 
         [Command("randomnumber")]
-        [Summary("Returns a random number between 1 and a maximum number.")]
+        [Summary("Returns a random number between 1 and a number.")]
         public async Task RandomNumber(int a)
         {
-            if (a < 1)
+            if (a < Int32.MinValue || a > Int32.MaxValue)
             {
-                var errembed = new EmbedBuilder
-                {
-                    Title = ":grey_question: Random Number - Error",
-                    Description = "The value must be greater than or equal to 1.",
-                    Color = 0xC25955,
-                    Footer = new EmbedFooterBuilder
-                    {
-                        Text = $"Response to {((IGuildUser)Context.User).Nickname ?? Context.User.Username}"
-                    }
-                };
-                await ReplyAsync(embed: errembed.Build());
+                await ReplyAsync(embed: GenerateError(
+                    $"Number must be between {Int32.MinValue} and {Int32.MaxValue}."
+                ));
+
                 return;
             }
 
-            Random rand = new Random(int.Parse(DateTime.Now.ToString("MMddHHmmss")));
-            var msgembed = new EmbedBuilder
-            {
-                Title = $":grey_question: Random Number - 1 to {a}",
-                Description = ((rand.Next() % a) + 1).ToString(),
-                Color = 0x9E845d,
-                Footer = new EmbedFooterBuilder { Text = $"Response to {((IGuildUser)Context.User).Nickname ?? Context.User.Username}" }
-            };
-            await ReplyAsync(embed: msgembed.Build());
+            int lower, upper, result;
+
+            lower = Math.Min(1, a);
+            upper = Math.Max(1, a);
+            result = GenerateResult(lower, upper);
+
+            await ReplyAsync(embed: GenerateEmbed(lower, upper, result));
         }
 
         [Command("randomnumber")]
         [Summary("Returns a random number between the lower value and upper value.")]
         public async Task RandomNumber(int a, int b)
         {
-            int lower, upper;
-
-            if (a == b)
+            if (a < Int32.MinValue || a > Int32.MaxValue || b < Int32.MinValue || b > Int32.MaxValue)
             {
-                var msgembed1 = new EmbedBuilder
-                {
-                    Title = $":grey_question: Random Number - {a} to {b}",
-                    Description = a.ToString(),
-                    Color = 0x9E845d,
-                    Footer = new EmbedFooterBuilder { Text = $"Response to {((IGuildUser)Context.User).Nickname ?? Context.User.Username}" }
-                };
-                await ReplyAsync(embed: msgembed1.Build());
-                await ReplyAsync(a.ToString());
+                await ReplyAsync(embed: GenerateError(
+                    $"Numbers must be between {Int32.MinValue} and {Int32.MaxValue}."
+                ));
+
                 return;
             }
-            
+
+            int lower, upper, result;
+
             lower = Math.Min(a, b);
             upper = Math.Max(a, b);
+            result = GenerateResult(lower, upper);
 
+            await ReplyAsync(embed: GenerateEmbed(lower, upper, result));
+        }
+
+        int GenerateResult(int lower, int upper)
+        {
             Random rand = new Random(int.Parse(DateTime.Now.ToString("MMddHHmmss")));
-            var msgembed = new EmbedBuilder
+            return (rand.Next() % ((upper + 1) - lower)) + lower;
+        }
+
+        Embed GenerateEmbed(int lower, int upper, int result)
+        {
+            EmbedBuilder embed = new EmbedBuilder
             {
                 Title = $":grey_question: Random Number - {lower} to {upper}",
-                Description = ((rand.Next() % ((upper + 1) - lower)) + lower).ToString(),
+                Description = $"{result}",
                 Color = 0x9E845d,
-                Footer = new EmbedFooterBuilder { Text = $"Response to {((IGuildUser)Context.User).Nickname ?? Context.User.Username}" }
+                Footer = new EmbedFooterBuilder
+                {
+                    Text = $"Response to {((IGuildUser)Context.User).Nickname ?? Context.User.Username}"
+                }
             };
-            await ReplyAsync(embed: msgembed.Build());
+
+            return embed.Build();
+        }
+
+        Embed GenerateError(string message)
+        {
+            EmbedBuilder embed = new EmbedBuilder
+            {
+                Title = ":grey_question: Random Number - Error",
+                Description = message,
+                Color = 0xC25955,
+                Footer = new EmbedFooterBuilder
+                {
+                    Text = $"Response to {((IGuildUser)Context.User).Nickname ?? Context.User.Username}"
+                }
+            };
+
+            return embed.Build();
         }
     }
 }
