22f5ae17a8ca6fd570be0e7f26a8ca3a9275e0e0
Vi Roesler <vroesler@leafpuddle.com>
2022-02-12 14:27:26 -0600

    Adding Npgsql package and a central Database class to handle connections and R/W operations.

---
+++ Database.cs
@@ -0,0 +1,12 @@
+using Npgsql;
+
+namespace Rover
+{
+    public class Database
+    {
+        string databaseName;
+        string databaseLocation;
+        string databaseUser;
+        string databasePassword;
+    }
+}
